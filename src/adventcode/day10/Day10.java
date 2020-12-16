aa192a1bc6546fc2dc7c0e0112d4b1526b484e68
Joan M. Tovar <jtovar@progracol.com>
2020-12-15 21:27:59 -0500

    Solution for Day 10.1

---
+++ src/adventcode/day10/Day10.java
@@ -0,0 +1,68 @@
+package adventcode.day10;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class Day10 {
+
+    public static ArrayList<Integer> adapters = new ArrayList<>();
+
+    public static HashMap<ArrayList<Integer>, Integer> cache = new HashMap<>();
+
+    public static void readFile(String filename){
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+            String line = reader.readLine();
+            while (line != null) {
+                adapters.add(Integer.valueOf(line));
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void findDifferences(){
+        Collections.sort(adapters);
+        System.out.println(adapters);
+
+        int i = -1, current = 0, dif1 = 0, dif2 = 0, dif3 = 1;
+
+        while (i < adapters.size() - 1){
+            int next = adapters.get(i+1);
+            int dif = next-current;
+
+            switch (dif){
+                case 1:
+                    dif1++;
+                    break;
+                case 2:
+                    dif2++;
+                    break;
+                case 3:
+                    dif3++;
+                    break;
+            }
+
+            i++;
+            current = adapters.get(i);
+        }
+
+        System.out.println("Result: " + (dif1*dif3));
+    }
+
+    public static void main(String[] args) {
+        readFile("/home/joan/dev/java/AdventCode/inputs/input_10.txt");
+
+        findDifferences();
+
+        adapters.add(0, 0);
+
+    }
+}
