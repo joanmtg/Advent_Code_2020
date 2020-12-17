9bd0d10e08c6e604b117bcfbc61c031fc7bf6c54
Joan M. Tovar <jtovar@progracol.com>
2020-12-16 19:36:26 -0500

    Solution for Day 10.2

--- src/adventcode/day10/Day10.java
+++ src/adventcode/day10/Day10.java
@@ -11,7 +11,7 @@ public class Day10 {
 
     public static ArrayList<Integer> adapters = new ArrayList<>();
 
-    public static HashMap<ArrayList<Integer>, Integer> cache = new HashMap<>();
+    public static HashMap<Integer, Long> cache = new HashMap<>();
 
     public static void readFile(String filename){
         BufferedReader reader;
@@ -57,12 +57,34 @@ public class Day10 {
         System.out.println("Result: " + (dif1*dif3));
     }
 
+    public static long combinations(int number){
+
+        if(!adapters.contains(number)){
+            return 0;
+        }
+
+        if(adapters.indexOf(number) == adapters.size() - 2){
+            return 1;
+        }
+
+        if(cache.containsKey(number)){
+            return cache.get(number);
+        }
+
+        cache.put(number, combinations(number+1) + combinations(number+2) + combinations(number+3));
+        return cache.get(number);
+    }
+
     public static void main(String[] args) {
         readFile("/home/joan/dev/java/AdventCode/inputs/input_10.txt");
 
         findDifferences();
 
         adapters.add(0, 0);
+        adapters.add(adapters.get(adapters.size()-1) + 3);
 
+        Collections.sort(adapters);
+
+        System.out.println("Result: " + combinations(0));
     }
 }
